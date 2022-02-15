34f457ba9572b62b231b9c7d4e1cfe5ca278758c
Benkeser <benkeser@emory.edu>
2022-02-15 14:46:08 -0500

    add glm for trt; remove SL.mean from SL library

--- sim3/code/run_sim.R
+++ sim3/code/run_sim.R
@@ -15,9 +15,9 @@ library(survtmle)
 library(SuperLearner)
 
 # SuperLearner library
-SL_library_ftime_ab <-  c("SL.glm", "SL.mean", "SL.earth", "SL.myglm")
-SL_library_ftime <-  c("SL.glm", "SL.mean", "SL.earth", "SL.myglm_noab")
-SL_library_other <- c("SL.glm", "SL.mean", "SL.earth", "SL.ranger")
+SL_library_ftime_ab <-  c("SL.glm", "SL.earth", "SL.myglm")
+SL_library_ftime <-  c("SL.glm", "SL.earth", "SL.myglm_noab")
+SL_library_other <- c("SL.glm", "SL.earth", "SL.ranger")
 
 # fitting function
 fitting = function(X, cens_rate, covid_rate, set_t0, study_stop, version){
@@ -77,7 +77,8 @@ fitting = function(X, cens_rate, covid_rate, set_t0, study_stop, version){
       t0 = set_t0,
       glm.ctime = "age + race + risk",
       SL.ftime = SL_library_ftime,
-      cvControl = list(V = 2)
+      cvControl = list(V = 2),
+      maxIter = 1
     )
     
     # estimate mediation parameters
@@ -92,12 +93,14 @@ fitting = function(X, cens_rate, covid_rate, set_t0, study_stop, version){
       mediatorSampProb = samp_prob,
       mediatorInCensMod = FALSE,
       t0 = set_t0,
+      glm.trt = "age + race + risk",
       glm.ctime = "age + race + risk",
       SL.ftime = SL_library_ftime_ab,
       glm.mediator = "age*race*risk",
       SL.trtMediator = SL_library_other,
       SL.eif = SL_library_other,
-      cvControl = list(V = 2)
+      cvControl = list(V = 2),
+      maxIter = 1
     )
   }
   
